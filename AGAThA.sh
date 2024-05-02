31fea45b7e8edcbd75446c8defd68a19c0bcb3e8
readwrite112 <readwrite112@gmail.com>
2024-05-02 14:21:09 +0900

    Updating AGAThA.sh

--- AGAThA.sh
+++ AGAThA.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-PROG="/agatha_ae/AGAThA/test_prog/"
-
-ITER=1 #number of iteration of each program
-IDLE=5 #sleep between iterations
-RAW_FILE="/agatha_ae/output/raw.log"
-FINAL_FILE="/agatha_ae/output/time.json"
-SCORE_FILE="/agatha_ae/output/score.log"
-DATASET_DIR="/agatha_ae/dataset/"
-DATASET_NAME="test"
+MAIN_DIR="/agatha_ae/"                  #the main directory
+PROG_DIR=$MAIN_DIR"AGAThA/test_prog/"   #the directory where the test program is
+OUTPUT_DIR=$MAIN_DIR"output/"           #the directory for the RAW_, FINAL_, SCORE_FILE
+DATASET_DIR=$MAIN_DIR"dataset/"         #the directory where the input dataset is located in
 FINAL_DIR=$PWD
-PROCESS="AGAThA"
+
+RAW_FILE=$OUTPUT_DIR"raw.log"           #stores all kernel exec. time of all iterations        
+FINAL_FILE=$OUTPUT_DIR"time.json"       #stores the average (total kernel exec. time of a single iteration)
+SCORE_FILE=$OUTPUT_DIR"score.log"       #stores the scores after alignment
+
+ITER=1                                  #number of iteration of each program
+IDLE=5                                  #sleep between iterations
+DATASET_NAME="test"                     #the name for the current dataset (will be shown in FINAL_FILE)
+PROCESS="AGAThA"                        #the process name (will be shown in FINAL_FILE)
 
 while getopts "i:" opt
 do
@@ -18,10 +21,11 @@ do
     esac
 done
 
-echo ">>> Running $PROCESS"
+mkdir -p $OUTPUT_DIR                    #creating the output directory
 
+echo ">>> Running $PROCESS for $ITER iterations."
 
-if [ -f $RAW_FILE ]; then
+if [ -f $RAW_FILE ]; then               #remove the output files before running the program
     rm $RAW_FILE
 fi
 
@@ -33,14 +37,18 @@ if [ -f $FINAL_FILE ]; then
     rm $FINAL_FILE
 fi
 
-iter=0
+iter=0                                  #start the main program
 while [ "$iter" -lt $ITER ] 
-do
-    ${PROG}manual -p -y local -a 1 -b 4 -q 6 -r 2 -z 400 -w 751 ${DATASET_DIR}ref.fasta ${DATASET_DIR}query.fasta > ${SCORE_FILE}
+do  
+    echo ">> Iteration $(($iter+1))"
+    ${PROG_DIR}manual -p -y local -a 1 -b 4 -q 6 -r 2 -z 400 -w 751 ${DATASET_DIR}ref.fasta ${DATASET_DIR}query.fasta > ${SCORE_FILE}
     ((iter++))
     sleep ${IDLE}s
 done
 
-python3 /agatha_ae/misc/avg_time.py $PROCESS $DATASET_NAME ${RAW_FILE} ${FINAL_FILE} $ITER  #get average exec. time across multiple iterations
+echo "$PROCESS complete."
+echo "Creating output files..."         #creating additional output files
 
+python3 /agatha_ae/misc/avg_time.py $PROCESS $DATASET_NAME ${RAW_FILE} ${FINAL_FILE} $ITER 
 
+echo "Complete."
